523cf758c75456c46a8068f74565fbd5be565601
YuanSaiNewComputer <yuans_work.com>
2023-06-26 13:36:32 +0800

    新的Vue项目

--- Vue/bmsv1/src/main.js
+++ Vue/bmsv1/src/main.js
@@ -1,5 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import { naive } from "naive-ui";
-createApp(App).use(naive)
-.mount('#app')
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
